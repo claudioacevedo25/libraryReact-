a70fc38f5fc755a729a8bf480f4a79b715055729
joel.albornoz@splight-ae.com <joel.albornoz@splight-ae.com>
2021-10-20 13:47:49 -0300

    [FAC-40] Republished library with all new changes

--- src/sui-library/dist/index.esm.js
+++ src/sui-library/dist/index.esm.js
@@ -4,68 +4,37 @@ import _slicedToArray from '@babel/runtime/helpers/slicedToArray';
 
 var e=[],t=[];function n(n,r){if(n&&"undefined"!=typeof document){var a,s=!0===r.prepend?"prepend":"append",d=!0===r.singleTag,i="string"==typeof r.container?document.querySelector(r.container):document.getElementsByTagName("head")[0];if(d){var u=e.indexOf(i);-1===u&&(u=e.push(i)-1,t[u]={}),a=t[u]&&t[u][s]?t[u][s]:t[u][s]=c();}else a=c();65279===n.charCodeAt(0)&&(n=n.substring(1)),a.styleSheet?a.styleSheet.cssText+=n:a.appendChild(document.createTextNode(n));}function c(){var e=document.createElement("style");if(e.setAttribute("type","text/css"),r.attributes)for(var t=Object.keys(r.attributes),n=0;n<t.length;n++)e.setAttribute(t[n],r.attributes[t[n]]);var a="prepend"===s?"afterbegin":"beforeend";return i.insertAdjacentElement(a,e),e}}
 
-var css$2 = ".buttonComponent {\r\n  border-radius: 3px;\r\n  padding: 0.3rem 0.5rem;\r\n  cursor: pointer;\r\n  border: none;\r\n  transition: all 0.3s ease-out;\r\n  box-shadow: #272727b0 1px 1px 1px, #272727b0 -1px -1px 1px;\r\n  background-color: var(--variant-background);\r\n  color:var(--variant-text-color);\r\n  margin: 5px;\r\n}\r\n.buttonComponent:hover {\r\n  box-shadow: #272727b0 1px 1px 3px, #272727b0 -1px -1px 3px;\r\n}\r\n.buttonComponent:active {\r\n  opacity: 0.8;\r\n}\r\n";
+var css$2 = ".buttonComponent {\r\n  border-radius: 3px;\r\n  padding: 0.3rem 0.5rem;\r\n  cursor: pointer;\r\n  border: none;\r\n  transition: all 0.3s ease-out;\r\n  box-shadow: #272727b0 1px 1px 1px, #272727b0 -1px -1px 1px;\r\n  color:var(--text-color);\r\n  margin: 5px;\r\n}\r\n.buttonComponent:hover {\r\n  box-shadow: #272727b0 1px 1px 3px, #272727b0 -1px -1px 3px;\r\n}\r\n.buttonComponent:active {\r\n  opacity: 0.8;\r\n}\r\n\r\n.buttonComponent__simple{\r\n  background-color: var(--primary);\r\n}\r\n.buttonComponent__alternate{\r\n  background-color: var(--secondary);\r\n}\r\n";
 n(css$2,{});
 
-var theme$1 = {
-  light: {
-    primary: '#F3F3F6',
-    background: '#FAFAFD',
-    secondary: '#F1F1F1',
-    'text-color': '#000000',
-    hierarchy1: '#B7FDA0',
-    hierarchy2: '#45B7BC',
-    hierarchy3: '#6AE8B0'
-  },
-  dark: {
-    background: '#000000',
-    primary: '#1B1621',
-    secondary: '#313238',
-    'text-color': '#FFFFFF',
-    hierarchy1: '#B7FDA0',
-    hierarchy2: '#45B7BC',
-    hierarchy3: '#6AE8B0'
-  }
-};
-
-var _excluded$1 = ["clickHandler", "variant", "mode"];
+var _excluded$1 = ["clickHandler", "variant"];
 
 var AwesomeButton = function AwesomeButton(_ref) {
   var clickHandler = _ref.clickHandler,
       variant = _ref.variant,
-      mode = _ref.mode,
       props = _objectWithoutProperties(_ref, _excluded$1);
 
   var children = props.children;
   return /*#__PURE__*/React.createElement("button", {
-    className: "buttonComponent",
-    style: {
-      '--variant-background': "".concat(theme$1[mode][variant]),
-      '--variant-text-color': "".concat(variant.indexOf('hierarchy') > -1 ? theme$1['light']['text-color'] : theme$1[mode]['text-color'])
-    },
+    className: "buttonComponent buttonComponent__".concat(variant),
     onClick: clickHandler
   }, children.toUpperCase());
 };
 
-var css$1 = "label{\r\n    color: var(--variant-text-color)\r\n}";
+var css$1 = "label{\r\n    color: var(--text-color)\r\n}";
 n(css$1,{});
 
-var _excluded = ["variant", "mode"];
+var _excluded = ["variant"];
 
 var AwesomeLabel = function AwesomeLabel(_ref) {
   _ref.variant;
-      var mode = _ref.mode,
-      props = _objectWithoutProperties(_ref, _excluded);
+      var props = _objectWithoutProperties(_ref, _excluded);
 
   var children = props.children;
-  return /*#__PURE__*/React.createElement("label", {
-    style: {
-      '--variant-text-color': "".concat(theme$1[mode]['text-color'])
-    }
-  }, children.toUpperCase());
+  return /*#__PURE__*/React.createElement("label", null, children.toUpperCase());
 };
 
-var css = ".theme_context_provider[theme=\"light\"]{\r\n    --primary: #F3F3F6;\r\n    --background: #FAFAFD;\r\n    --secondary: #F1F1F1;\r\n    --text-color:#000000;\r\n    --hierarchy1:#B7FDA0;\r\n    --hierarchy2:#45B7BC;\r\n    --hierarchy3:#6AE8B0;\r\n  }\r\n  \r\n  .theme_context_provider[theme=\"dark\"]{\r\n    --background:#000000;\r\n    --primary:#1B1621;\r\n    --secondary: #313238;\r\n    --text-color:#FFFFFF;\r\n    --hierarchy1:#B7FDA0;\r\n    --hierarchy2:#45B7BC;\r\n    --hierarchy3:#6AE8B0;\r\n  }";
+var css = ".theme_context_provider[theme=\"light\"]{\r\n    --background: #FAFAFD;\r\n    --primary: #F3F3F6;\r\n    --secondary: #F1F1F1;\r\n    --text-color:#000000;\r\n    --hierarchy1:#B7FDA0;\r\n    --hierarchy2:#45B7BC;\r\n    --hierarchy1:#6AE8B0;\r\n  }\r\n  \r\n  .theme_context_provider[theme=\"dark\"]{\r\n    --background:#000000;\r\n    --primary:#1B1621;\r\n    --secondary: #313238;\r\n    --text-color:#FFFFFF;\r\n    --hierarchy1:#B7FDA0;\r\n    --hierarchy2:#45B7BC;\r\n    --hierarchy3:#6AE8B0;\r\n  }";
 n(css,{});
 
 var ThemeContext = /*#__PURE__*/createContext();
@@ -93,8 +62,8 @@ var ThemeContextProvider = function ThemeContextProvider(_ref) {
 
 var theme = {
   light: {
-    primary: '#F3F3F6',
     background: '#FAFAFD',
+    primary: '#F3F3F6',
     secondary: '#F1F1F1',
     'text-color': '#000000',
     hierarchy1: '#B7FDA0',
