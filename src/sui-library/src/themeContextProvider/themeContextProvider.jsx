eb7afc174b9db847048c0d907f59d57f814f9a8b
joel.albornoz@splight-ae.com <joel.albornoz@splight-ae.com>
2021-10-25 12:02:15 -0300

    [FAC-40] Background Color transpased to ThemeContextProvider

--- src/sui-library/src/themeContextProvider/themeContextProvider.jsx
+++ src/sui-library/src/themeContextProvider/themeContextProvider.jsx
@@ -1,14 +1,25 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import "./themeContextProvider.css"
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = ({ children }) => {
   const [mode, setTheme] = useState("dark");
+
+  useEffect(() => {
+    const savedTheme =JSON.parse(localStorage.getItem("themeMode"))
+    savedTheme && setTheme(savedTheme)
+  }, [])
+
+  const toggleTheme = () => {
+    localStorage.setItem("themeMode", JSON.stringify(mode === "dark" ? "light" : "dark"))
+    setTheme(mode === "dark" ? "light" : "dark")
+  }
+
   return (
     <ThemeContext.Provider
       value={{
         mode,
-        setTheme: () => setTheme(mode === "dark" ? "light" : "dark")
+        setTheme: toggleTheme
       }}
     >
       <div theme={mode}
