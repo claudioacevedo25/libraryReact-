a56832fa824909529b66b2dfeb919a46a8841747
Maxi <maximiliano.pezzotta@splight-ae.com>
2021-10-27 09:26:49 -0300

    fac-69-icon: export new component

--- src/sui-library/src/index.js
+++ src/sui-library/src/index.js
@@ -1,8 +1,18 @@
 import Button from "./button/button.jsx";
 import Typography from "./typography/typography.js";
+import SplightName from "./icons/splightName.jsx";
+import SplightRounded from "./icons/splightRounded.jsx";
 import ThemeContextProvider, {ThemeContext} from "./themeContextProvider/themeContextProvider.jsx"
 import theme from './ThemeContextProvider/theme.js'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 
-export default { Button, Typography, ThemeContextProvider, ThemeContext, theme };
+export default { 
+    Button, 
+    Typography, 
+    ThemeContextProvider, 
+    ThemeContext, 
+    theme,
+    SplightRounded,
+    SplightName
+ };
