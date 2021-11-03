e81ad0ca72908577153f49ccc43f9008ef82280d
Maxi <maximiliano.pezzotta@splight-ae.com>
2021-11-03 14:49:25 -0300

    fac-64-header: remove duplicate typography

--- src/App.js
+++ src/App.js
@@ -76,34 +76,9 @@ function App() {
           src="https://profile-pictures-test.s3.amazonaws.com/fa06a8891d1a4316992d4197cd2869ea.jpg"
           name="Thomas "
         />
-        <Typography variant="h3">Daniel moore</Typography>
-        <Button >My Profile</Button>
+            <Typography variant="h3">Daniel moore</Typography>
+            <Button >My Profile</Button>
         </Popover>
-      <div className="App__labels">
-        <Typography variant="h1">Label h1</Typography>
-        <Typography variant="h2">Label h2</Typography>
-        <Typography variant="h3">Label h3</Typography>
-        <Typography variant="subtitle">Label subtitle</Typography>
-        <Typography variant="body">Label body</Typography>
-        <Typography variant="bodyBold">Label bodyBold</Typography>
-        <Typography variant="small">Label small</Typography>
-        <Typography variant="preTitle">Label preTitle</Typography>
-        <Typography variant="textButton">Label textButton</Typography>
-        <Typography>No variant</Typography>
-        <Typography variant="body" color="red" size="1.8">
-          Color and size
-        </Typography>
-        <label>ultimo</label>
-      </div>
-     
-      <div className="App__avatar">
-        <Avatar
-          src="https://profile-pictures-test.s3.amazonaws.com/fa06a8891d1a4316992d4197cd2869ea.jpg"
-          name="Thomas "
-        />
-        <Avatar name="Estrella cholod" />
-      </div>
-      
     </div>
     </>
   );
