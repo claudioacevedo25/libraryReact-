6df314d390091c53c6ba6abd6770ad7bfae57b94
Estrella <estrella.cholod@splight-ae.com>
2021-11-15 14:17:38 -0300

    fac-79-userCard add export userCard

--- src/sui-library/src/index.js
+++ src/sui-library/src/index.js
@@ -9,6 +9,7 @@ import Popover from "./popover/popover.jsx";
 import Header from "./header/header.jsx";
 import Spinner from "./spinner/spinner.jsx";
 import BasicCard from "./basicCard/basicCard.jsx";
+import UserCard from "./userCard/userCard.jsx";
 import ThemeContextProvider, {ThemeContext} from "./themeContextProvider/themeContextProvider.jsx"
 import theme from './themeContextProvider/theme.js';
 
@@ -29,4 +30,5 @@ export default {
     Header,
     Spinner,
     BasicCard,
+    UserCard,
  };
